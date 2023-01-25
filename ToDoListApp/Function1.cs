96a6a3316eba982ca3628fd8e90e826f816062c7
Alessandro Tucci <alessandro.tucci@aesys.tech>
2023-01-25 17:53:12 +0100

    Post and Get

--- ToDoListApp/Function1.cs
+++ ToDoListApp/Function1.cs
@@ -13,20 +13,46 @@ using System.Linq;
 using ToDoListApp.Models;
 using Microsoft.SqlServer.Server;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ToDoListApp
 {
     public static class Function1
     {
-        [FunctionName("InMemory_GetTodos")]
+        [FunctionName("Db_GetTodo")]
+        public static IActionResult GetTodoFromDb(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "tododb/{id?}")] HttpRequest req, ILogger log, string id,
+            [Sql(@"IF (@id IS NULL or @id = '') SELECT * FROM [ToDo].[dbo].[Todos] ELSE SELECT * FROM [ToDo].[dbo].[Todos] WHERE Id = @id", Parameters = "@id={id}", CommandType = System.Data.CommandType.Text, ConnectionStringSetting = "ConnectionString")] IEnumerable<Todo> todos)
+        {
+
+            return new OkObjectResult(todos);
+        }
+
+        [FunctionName("Db_CreateTodo")]
+        public static async Task<IActionResult> CreateTodoDb(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "tododb")] HttpRequest req, ILogger log,
+            [Sql("Todos", ConnectionStringSetting = "ConnectionString")] IAsyncCollector<Todo> todos)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<Todo>(requestBody);
+
+            await todos.AddAsync(input);
+            await todos.FlushAsync();
+
+            return new OkResult();
+        }
+
+        [FunctionName("Cache_GetTodos")]
         public static IActionResult GetTodos(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="todo")]HttpRequest req, ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")] HttpRequest req, ILogger log)
         {
             log.LogInformation("Getting todo list items");
             return new OkObjectResult(TodoApiInMemory.todoList);
         }
 
-        [FunctionName("InMemory_GetTodoById")]
+        [FunctionName("Cache_GetTodoById")]
         public static IActionResult GetTodoById(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")] HttpRequest req, ILogger log, string id)
         {
@@ -36,13 +62,13 @@ namespace ToDoListApp
             {
                 return new NotFoundResult();
             }
-            
+
             return new OkObjectResult(todo);
         }
 
-        [FunctionName("InMemory_CreateTodo")]
+        [FunctionName("Cache_CreateTodo")]
         public static async Task<IActionResult> CreateTodo(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")]HttpRequest req, ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req, ILogger log)
         {
             log.LogInformation("Creating a new todo list item");
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
@@ -54,21 +80,8 @@ namespace ToDoListApp
 
         }
 
-        [FunctionName("InDb_CreateTodo")]
-        public static async Task<IActionResult> CreateTodoDb(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "tododb")]HttpRequest req, ILogger log,
-            [Sql("Todos", ConnectionStringSetting = "ConnectionString")] IAsyncCollector<Todo> todos)
-        {
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var input = JsonConvert.DeserializeObject<Todo> (requestBody);
 
-            await todos.AddAsync(input);
-            await todos.FlushAsync();
-
-            return new OkResult();
-        }
-
-        [FunctionName("InMemory_UpdateTodo")]
+        [FunctionName("Cache_UpdateTodo")]
         public static async Task<IActionResult> UpdateTodo([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")] HttpRequest req, ILogger log, string id)
         {
             var todo = TodoApiInMemory.todoList.FirstOrDefault(t => t.Id == id);
@@ -78,7 +91,7 @@ namespace ToDoListApp
                 return new NotFoundResult();
             }
 
-            string requestBody = await new StreamReader (req.Body).ReadToEndAsync();
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var updated = JsonConvert.DeserializeObject<TodoUpdateModel>(requestBody);
 
             todo.IsCompleted = updated.IsCompleted;
@@ -91,8 +104,8 @@ namespace ToDoListApp
             return new OkObjectResult(todo);
         }
 
-        [FunctionName("InMemory_DeleteTodo")]
-        public static IActionResult DeleteTodo([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")]HttpRequest req, ILogger log, string id)
+        [FunctionName("Cache_DeleteTodo")]
+        public static IActionResult DeleteTodo([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")] HttpRequest req, ILogger log, string id)
         {
             var todo = TodoApiInMemory.todoList.FirstOrDefault(t => t.Id == id);
             if (todo == null)
